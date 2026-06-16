2a5826ed944270f7219e9144346c87dcf51ed719
sithslayer337 <barqavuis@gmail.com>
2026-06-15 21:24:38 -0700

    Create players.js

---
+++ players.js
@@ -0,0 +1,8 @@
+const PLAYERS = [
+  { id: 1, name: "Zephyr_X",   civ: 2100, hg: 1800, uhc: 1520 },
+  { id: 2, name: "NovaBlade",  civ: 1500, hg: 1200, uhc: 1100 },
+  { id: 3, name: "CrimsonAce", civ: 1000, hg: 900,  uhc: 850  },
+
+  // Add players here:
+  // { id: 4, name: "YourName", civ: 0, hg: 0, uhc: 0 },
+];
