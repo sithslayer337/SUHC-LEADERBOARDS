3af8bf42d58996f85d957f917fd1a39a6b0be077
sithslayer337 <barqavuis@gmail.com>
2026-07-13 21:06:49 -0700

    Update capes.js

--- capes.js
+++ capes.js
@@ -1,2 +1,8 @@
-//coming soon
-{}
+const invitedPlayers = ["sithslayer337", "Ytuehba", "JettSilverr", "RainsAK", "Itzrealvexy", "Boogieman881", "Verxza x", "Bread4Bone", "BattierCrown747", "Xxzert1050", "SwingMCC", "s1thslxyer33", "BlitsyMC", "madchad20081149", "Itzrealvexy", "BattierCrown747"];
+const boosterPlayers = ["sithslayer337", "Itzrealvexy", "Ytuehba", "PlncheBot", "Kaiistooicy", "ItzCazry", "s1thslxyer33", "Twishsbd"];
+const ogPlayers = ["PlinkO34", "sithslayer337", "Itzrealvexy", "Ytuehba", "PlncheBot", "Kaiistooicy", "Verxza x", "BPKOfficial", "OasisGamer03", "s1thslxyer33", "SwingMCC"];
+const menacingPlayers = ["Killtimebtw", "BloodHound16333", "s1thslxyer33", "sithslayer337", "SweetPup5752066"];
+const uhcPlayers = ["Cease Now", "Trentdapro1", "s1thslxyer33", "sithslayer337", "Jackson1232246", "Trentdapro2"];
+const contributorPlayers = ["s1thslxyer33", "FatherAJT", "Verxza x", "Itzrealvexy", "Boogieman881", "sithslayer337", "missedV3", "missedV67"];
+const supporterPlayers = ["s1thslxyer33", "Itzrealvexy", "madchad20081149", "Kaiistooicy", "Xxzert1050", "sithslayer337"];
+const civPlayers = ["s1thslxyer33", "sithslayer337", "justkqi", "MrFxber", "TroddenToast412", "aeryrisu", "TecTonic7124", "Bread4Bone", "BOBJR789", "ItsMini Tower", "Doomer99218"];
